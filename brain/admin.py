b119ccd709cddf384993c70515ef610a0e0e563c
Soldat313 <119010959+Soldat313@users.noreply.github.com>
2022-12-18 16:37:28 +0300

    Update admin.py

--- brain/admin.py
+++ brain/admin.py
@@ -10,13 +10,3 @@ User = get_user_model()
 class UserAdmin(UserAdmin):
     pass
 
-
-# from django.contrib import admin
-# from django.contrib.auth import get_user_model
-# from django.contrib.auth.admin import UserAdmin
-#
-# User = get_user_model()
-#
-# @admin.register(User)
-# class UserAdmin(UserAdmin):
-#     pass
